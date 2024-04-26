799709b888439df21c4d91a99d20c25462922ee5
Toaruu <145032179+Toaruu@users.noreply.github.com>
2024-04-27 00:41:40 +1000

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,10 @@
+function greet(name) {
+    alert('Hello, ' + name);
+}
+greet('Alice');
+
+let txt = "Hello World!";
+txt = txt.upper
+
+let txt1 = "I can eat bananas all day";
+let x = txt1.slice(10, 17)
